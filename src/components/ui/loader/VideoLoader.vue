5e7a8661423a4be0f9345ffe51473d282b2a5505
moshcat <mauricecathoverile@gmail.com>
2025-02-07 14:30:15 +0800

    feat(vid): add a video loader for optimization

---
+++ src/components/ui/loader/VideoLoader.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="animate-pulse mb-16 rounded-lg bg-gray-300 aspect-video max-w-2xl lg:max-w-4xl xl:max-w-6xl w-full mx-auto flex items-center justify-center"
+  ></div>
+</template>
