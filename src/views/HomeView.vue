7b44b5f64d1736ababd6a46e016cc2c9973fd773
moshcat <mauricecathoverile@gmail.com>
2025-02-07 14:29:46 +0800

    feat(vid): add a video section on home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
 import Hero from '@/components/pages/Home/Hero.vue'
+import VideoLoader from '@/components/ui/loader/VideoLoader.vue'
+
+const HeroVideo = defineAsyncComponent({
+  loader: () => import('@/components/pages/Home/HeroVideo.vue'),
+  delay: 1000,
+  timeout: 3000,
+  suspensible: true,
+})
 </script>
 
 <template>
   <main>
+    <!--    hero section -->
     <Hero />
+
+    <!--    video section-->
+    <Suspense>
+      <template #default>
+        <HeroVideo />
+      </template>
+      <template #fallback>
+        <VideoLoader />
+      </template>
+    </Suspense>
+    <!--    end video section -->
   </main>
 </template>
