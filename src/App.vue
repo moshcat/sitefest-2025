b2b80a174ed5b1b51e8d0484586c1c64d7442d86
moshcat <mauricecathoverile@gmail.com>
2025-02-08 21:41:08 +0800

    feat(loader): add a page loader

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
+import { ref } from 'vue'
 import MainNav from '@/components/layout/MainNav.vue'
 import MainFooter from '@/components/layout/MainFooter.vue'
+import PageLoader from '@/components/ui/loader/PageLoader.vue'
+
+const isLoading = ref(false)
+const router = useRouter()
+
+router.beforeEach((to, from, next) => {
+  isLoading.value = true
+  next()
+})
+
+router.afterEach((to, from, next) => {
+  setTimeout(() => (isLoading.value = false), 800)
+})
 </script>
 
 <template>
   <div class="font-figtree">
     <MainNav />
+    <Transition name="fade">
+      <PageLoader v-if="isLoading" />
+    </Transition>
     <RouterView />
     <MainFooter />
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.8s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
