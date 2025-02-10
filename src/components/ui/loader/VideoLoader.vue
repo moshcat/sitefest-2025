a98fdfc6e03bf1c59bfddcf897235facf959f8fe
moshcat <mauricecathoverile@gmail.com>
2025-02-10 14:24:56 +0800

    feat(loader: fix loader video width

--- src/components/ui/loader/VideoLoader.vue
+++ src/components/ui/loader/VideoLoader.vue
@@ -2,6 +2,6 @@
 
 <template>
   <div
-    class="animate-pulse mb-16 rounded-lg bg-gray-300 aspect-video max-w-2xl lg:max-w-4xl xl:max-w-6xl w-full mx-auto flex items-center justify-center"
+    class="animate-pulse mb-16 rounded-lg bg-gray-300 aspect-video max-w-[20rem] sm:max-w-[36rem] md:max-w-2xl lg:max-w-[58rem] xl:max-w-6xl mx-auto flex items-center justify-center"
   ></div>
 </template>
